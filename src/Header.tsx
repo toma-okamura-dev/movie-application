cc27d08a2615e9f5d978c029cf1c3f9887d1e9bc
Toma Okamura <toma.okamura.job@gmail.com>
2025-10-12 21:01:05 +0900

    タイトルの変更を加えました

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ function Header({ children }: { children: React.ReactNode }) {
     return (
       <div className="app-bg">
         <header className="app-header">
-          <h1 className="app-title">MOVIEFLIX</h1>
+          <h1 className="app-title">NetFlixみたいなアプリ</h1>
         </header>
         <main>{children}</main>
       </div>
